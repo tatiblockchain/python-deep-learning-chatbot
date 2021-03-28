a93eebb5f8f671d94470539319ad321ee4f70ee1
Tati Blockchain <43959395+tatiblockchain@users.noreply.github.com>
2021-03-28 20:09:47 +0200

    Add files via upload

---
+++ python-deep-learning-chatbot/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, jsonify, request
+import processor
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'enter-a-very-secretive-key-3479373'
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return render_template('index.html', **locals())
+
+
+
+@app.route('/chatbot', methods=["GET", "POST"])
+def chatbotResponse():
+
+    if request.method == 'POST':
+        the_question = request.form['question']
+
+        response = processor.chatbot_response(the_question)
+
+    return jsonify({"response": response })
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8888', debug=True)
